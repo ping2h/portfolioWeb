b2ec9d85b43e38b64b96dfb89780c56cb2f412be
abc <754956348a@gmail.com>
2025-07-25 12:59:59 +0800

    test

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -2,8 +2,11 @@ import accountService from '../services/accountService.js';
 
 export function deposit(req, res) {
   try {
+    console.log("进入了deposit controller!!");
     const { id, amount } = req.body;
+    console.log(`id: ${id}, amount: ${amount}`);
     const acc = accountService.deposit(id, amount);
+    console.log("储存完成");
     res.json(acc);
   } catch (err) {
     res.status(400).json({ error: err.message });
