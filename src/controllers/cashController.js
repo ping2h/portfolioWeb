abeb097bd14761163fc92be4100c6f0d82e30401
abc <754956348a@gmail.com>
2025-07-29 11:34:59 +0800

    feat dev transcation and cash flow

---
+++ src/controllers/cashController.js
@@ -0,0 +1,12 @@
+﻿import * as cashService from '../services/cashService.js';
+
+export const getIncomeByRecentMonths = async (req, res) => {
+    try {
+        const months = Number(req.params.months) || 1;
+        const result = await cashService.getCashIncomeByRecentMonths(months);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
